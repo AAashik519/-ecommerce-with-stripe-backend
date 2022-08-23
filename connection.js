0422398bbb12f3876f270ef8c732ef4ff1fd2bd4
AAashik519 <64531121+AAashik519@users.noreply.github.com>
2022-08-24 03:33:59 +0600

    User Model & Routes

--- connection.js
+++ connection.js
@@ -1,6 +1,13 @@
- require('dotenv')
+ require('dotenv').config()
  const mongoose= require('mongoose');
- const connectionStr=''
- mongoose.connect(connectionStr ,{useNewParser:true })
+
+ const connectionStr=`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.faozt.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`
+
+ mongoose.connect(connectionStr ,{useNewUrlparser: true})
  .then(()=>console.log("connected to mongoDb") )
- .catch((err)=> console.log(err))
+ .catch(err => console.log(err))
+
+
+ mongoose.connection.on('error', err=>{
+    console.log('err')
+ })
